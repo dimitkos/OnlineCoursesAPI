f30c3f0355290ff384da9828db74cef995b404a8
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-06 09:59:53 +0300

    Remove from partial class instructor thr interface

--- OnlineCourses.Implementation/DataBaseImplementation/OnCoursesImplementation.Instructors.cs
+++ OnlineCourses.Implementation/DataBaseImplementation/OnCoursesImplementation.Instructors.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace OnlineCourses.Implementation.DataBaseImplementation
 {
-    public partial class OnCoursesImplementation :IService
+    public partial class OnCoursesImplementation 
     {
         public GetInstructorsResponse GetInstructors()
         {
