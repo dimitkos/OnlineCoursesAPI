151bec65dd0700b86c6e9e7defca316b0184eada
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-06 09:04:24 +0300

    Add instructor to DBTypes and and the response

---
+++ OnlineCourses.Types/Responses/GetInstructorsResponse.cs
@@ -0,0 +1,17 @@
+﻿using OnlineCourses.Types.DbTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourses.Types.Responses
+{
+    [DataContract]
+    public class GetInstructorsResponse
+    {
+        [DataMember(Name = "instructors")]
+        public IEnumerable<Instructor> Instructors { get; set; }
+    }
+}
