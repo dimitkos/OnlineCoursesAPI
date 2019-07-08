ccaf954b816f3f2a0c99f1dd0a3bfef905482e69
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-08 22:55:27 +0300

    chande property in category class

--- OnlineCourses.Types/DbTypes/Category.cs
+++ OnlineCourses.Types/DbTypes/Category.cs
@@ -10,8 +10,8 @@ namespace OnlineCourses.Types.DbTypes
     [DataContract]
     public class Category
     {
-        [DataMember(Name = "id")]
-        public int Id { get; set; }
+        [DataMember(Name = "categoryId")]
+        public string CategoryId { get; set; }
 
         [DataMember(Name = "name")]
         public string Name { get; set; }
