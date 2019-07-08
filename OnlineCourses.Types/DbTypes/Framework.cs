efd68764a1d62d651a895dd384afbfa887a15161
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-08 22:57:47 +0300

    add framework to db types

---
+++ OnlineCourses.Types/DbTypes/Framework.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourses.Types.DbTypes
+{
+    [DataContract]
+    public class Framework
+    {
+        [DataMember(Name = "frameworkId")]
+        public string FrameworkId { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+    }
+}
