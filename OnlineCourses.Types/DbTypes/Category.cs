e5a474c5727fc865287f7b7d3fb23bc01450d1cb
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-08 22:53:48 +0300

    add category to dbtypes

---
+++ OnlineCourses.Types/DbTypes/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourses.Types.DbTypes
+{
+    [DataContract]
+    public class Category
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+    }
+}
