98ffa68b9d6e453ef6f20c0d831566e15014f664
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-21 19:05:31 +0300

    add getframeworksresponse class

---
+++ OnlineCourses.Types/Responses/GetFrameworksResponse.cs
@@ -0,0 +1,17 @@
+﻿using OnlineCourses.Types.DbTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourses.Types.Responses
+{
+    [DataContract]
+    public class GetFrameworksResponse
+    {
+        [DataMember(Name = "frameworks")]
+        public IEnumerable<Framework> Frameworks { get; set; }
+    }
+}
