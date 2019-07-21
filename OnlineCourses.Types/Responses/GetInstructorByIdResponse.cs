2506ffdf7dba36cdc32e979811c11941a4c145fa
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-21 20:06:52 +0300

    create getinstructorbyid response and request

---
+++ OnlineCourses.Types/Responses/GetInstructorByIdResponse.cs
@@ -0,0 +1,17 @@
+﻿using OnlineCourses.Types.DbTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCourses.Types.Responses
+{
+    [DataContract]
+    public class GetInstructorByIdResponse
+    {
+        [DataMember(Name = "instructor")]
+        public Instructor Instructors { get; set; }
+    }
+}
