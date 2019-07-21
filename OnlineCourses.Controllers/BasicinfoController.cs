197be57d3e5c2370182aaed9bb4430c936109581
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-07-21 18:57:10 +0300

    Add basic info controller

---
+++ OnlineCourses.Controllers/BasicinfoController.cs
@@ -0,0 +1,21 @@
+﻿using OnlineCourses.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OnlineCourses.Controllers
+{
+    public class BasicinfoController : ApiController
+    {
+        
+        private readonly IService service;
+
+        public BasicinfoController(IService service)
+        {
+            this.service = service;
+        }
+    }
+}
