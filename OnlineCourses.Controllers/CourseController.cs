46922a15cfcd792e22b3aaa1193d016105932f6d
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-10-20 16:39:17 +0300

    create course controller

---
+++ OnlineCourses.Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using OnlineCourses.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OnlineCourses.Controllers
+{
+    public class CourseController : ApiController
+    {
+        private readonly IService service;
+
+        public CourseController(IService service)
+        {
+            this.service = service;
+        }
+    }
+}
